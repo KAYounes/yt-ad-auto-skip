89cf67b1feee460f1298fb97bd2f4ade5626ab54
Kareem Amr Younes <official.kyounes@gmail.com>
2024-02-04 08:58:30 +0200

    Remove previous playback speed restortion

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,6 @@
 let options = { adPlaybackSpeed: 10, adMute: true };
-const video = document.querySelector('video');
-let prevPlaybackRate = video.playbackRate;
+let video = document.querySelector('video');
+// let prevPlaybackRate = video.playbackRate;
 
 console.log(`
 \n\n\n
@@ -37,32 +37,28 @@ chrome.storage.onChanged.addListener((changes, area) => {
 getOptions();
 
 const observer = new MutationObserver((mutations) => {
-  console.log(
-    `
-    Sometimes site loads before teh <video> tag, so the querySelector fails.
-    Right now the video tag is
-    `,
-    video,
-  );
-  console.log(options);
-
-  prevPlaybackRate = video.playbackRate === 10 ? prevPlaybackRate : video.playbackRate;
+  if (video === null) video = document.querySelector('video');
   let adPlaying = document.querySelector('.video-ads.ytp-ad-module')?.hasChildNodes() ?? false;
-
-  let allAdText = [...document.querySelectorAll('*[id^=ad-text]')];
-
-  let countDown = allAdText.filter((e) => ['1', '2', '3', '4', '5'].includes(e.innerText))[0];
-  let skip = allAdText.filter((e) => e.innerText === 'Skip')[0];
+  let adTextNodes = [...document.querySelectorAll('*[id^=ad-text]')];
+  let countDown = adTextNodes.filter((e) => ['1', '2', '3', '4', '5'].includes(e.innerText))[0];
+  let skip = adTextNodes.filter((e) => e.innerText === 'Skip')[0];
+  console.log({ adTextNodes, countDown, skip });
+  console.log({ video, options });
 
   if (adPlaying) {
-    video.muted = true;
-    video.playbackRate = 0.5;
-    console.log(`Ad Is Playing Now`);
+    video.muted = options.adMute;
+    video.playbackRate = options.adPlaybackSpeed;
+    console.log(`${'='.repeat(20)}\nAd Is Playing Now\n${'='.repeat(20)}\n`);
+    if (skip) {
+      skip.click();
+    }
     // setTimeout(function () {
     //   video.muted = false;
     //   skip.click();
     //   video.playbackRate = prevPlaybackRate;
     // }, 200);
+  } else {
+    prevPlaybackRate = video.playbackRate === 10 ? prevPlaybackRate : video.playbackRate;
   }
 });
-observer.observe(document.querySelector('.html5-video-player    '), { childList: true, subtree: true }); // Observe entire document for changes
+observer.observe(document.querySelector('.html5-video-player'), { childList: true, subtree: true }); // Observe entire document for changes
