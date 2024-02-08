bea161a58480e827390e6eaa89e83ac7d328bc75
Kareem Amr Younes <official.kyounes@gmail.com>
2024-02-08 09:12:21 +0200

    Fix query selector returning null

--- scripts/content.js
+++ scripts/content.js
@@ -1,33 +1,13 @@
 let options = { adPlaybackSpeed: 10, adMute: true };
 let video = document.querySelector('video');
-// let prevPlaybackRate = video.playbackRate;
 
-console.log(`
-\n\n\n
-FIRST RUN
-FIRST RUN
-FIRST RUN
-FIRST RUN
-FIRST RUN
-FIRST RUN
-FIRST RUN
-FIRST RUN
-FIRST RUN
-FIRST RUN
-FIRST RUN
-FIRST RUN
-FIRST RUN
-FIRST RUN
-\n\n\n
-`);
-
+// Listen for configuration changes
 async function getOptions() {
   options = await chrome.storage.sync.get(Object.keys(options), (r) => (options = r));
 }
 
 chrome.storage.onChanged.addListener((changes, area) => {
   if (area === 'sync') {
-    console.log('>>> ', changes);
     for (let key in changes) {
       options[key] = changes[key].newValue;
     }
@@ -35,15 +15,28 @@ chrome.storage.onChanged.addListener((changes, area) => {
 });
 
 getOptions();
+// END
 
-const observer = new MutationObserver((mutations) => {
+// Attach Obserbers
+const adObserver = new MutationObserver(handleAd);
+
+// Wait for #ytd-player to load into the DOM
+const documentObserver = new MutationObserver(function (mutations, obs) {
+  const ytdPlayer = document.querySelector('#ytd-player');
+  if (ytdPlayer == null) return;
+
+  adObserver.observe(ytdPlayer, { childList: true, subtree: true });
+  documentObserver.disconnect();
+});
+
+documentObserver.observe(document, { childList: true, subtree: true });
+
+function handleAd() {
   if (video === null) video = document.querySelector('video');
   let adPlaying = document.querySelector('.video-ads.ytp-ad-module')?.hasChildNodes() ?? false;
   let adTextNodes = [...document.querySelectorAll('*[id^=ad-text]')];
   let countDown = adTextNodes.filter((e) => ['1', '2', '3', '4', '5'].includes(e.innerText))[0];
   let skip = adTextNodes.filter((e) => e.innerText === 'Skip')[0];
-  console.log({ adTextNodes, countDown, skip });
-  console.log({ video, options });
 
   if (adPlaying) {
     video.muted = options.adMute;
@@ -52,13 +45,5 @@ const observer = new MutationObserver((mutations) => {
     if (skip) {
       skip.click();
     }
-    // setTimeout(function () {
-    //   video.muted = false;
-    //   skip.click();
-    //   video.playbackRate = prevPlaybackRate;
-    // }, 200);
-  } else {
-    prevPlaybackRate = video.playbackRate === 10 ? prevPlaybackRate : video.playbackRate;
   }
-});
-observer.observe(document.querySelector('.html5-video-player'), { childList: true, subtree: true }); // Observe entire document for changes
+}
