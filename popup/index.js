aea4a48816d88613e663ec413aac3c8454195c78
Kareem Amr Abdelsamie Abdelrahman Younes <official.kyounes@gmail.com>
2024-04-13 23:01:21 +0200

    Add version detection

--- popup/index.js
+++ popup/index.js
@@ -1,6 +1,59 @@
 import { setStorageSync, getStorageSync } from './../scripts/storage.js';
 // initial options
 let options = JSON.parse(JSON.stringify(GLOBALS.settings));
+let manafist = null;
+let repoRelease = null;
+
+async function fetchJSON(url, options = {}) {
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    console.log(`HTTP error! Status: ${response.status}`);
+    return '{}';
+  }
+  return response.json();
+}
+
+async function fetchManifest() {
+  const url = chrome.runtime.getURL('../manifest.json');
+  return fetchJSON(url);
+}
+
+async function fetchRepoRelease() {
+  const url = 'https://api.github.com/repos/KAYounes/yt-ad-auto-skip/releases/latest';
+  const options = {
+    headers: { Accept: 'application/vnd.github.v3+json' },
+  };
+  return fetchJSON(url, options);
+}
+
+async function checkForUpdates() {
+  try {
+    const [manifest, repoRelease] = await Promise.all([fetchManifest(), fetchRepoRelease()]);
+    console.log({ manifest, repoRelease });
+    const manafistVersion = extractVersion(manifest.version);
+    const repoVersion = extractVersion(repoRelease.tag_name);
+
+    if (manafistVersion === '' || repoVersion === '') return;
+
+    const updateFound = isUpdateFound(repoVersion, manafistVersion);
+    console.log(updateFound);
+
+    if (!updateFound) return;
+
+    const updatesDiv = document.querySelector('.updates');
+    const messageSpan = document.querySelector('.updates > .message');
+    const versionSpan = document.querySelector('.updates > .version');
+    updatesDiv.classList.remove('hide');
+    messageSpan.textContent = `A new ${updateFound.toUpperCase()} update is found`;
+    versionSpan.textContent = `v${repoVersion}`;
+
+    return;
+  } catch (error) {
+    console.log('Error checking for updates', error);
+  }
+}
+
+checkForUpdates();
 
 // look for options in storage
 let storage = await getStorageSync(Object.keys(options)).then(function (result) {
@@ -69,4 +122,29 @@ function optionKeysWasModified(storage, options) {
   }
 
   return false;
+}
+
+function isUpdateFound(remote, local) {
+  // Helper function to clean and parse version string to an array of numbers
+  const remoteVersionSemantic = remote.split('.').map(Number);
+  const localVersionSemantic = local.split('.').map(Number);
+  const diff = remoteVersionSemantic.map((e, i) => e - localVersionSemantic[i]);
+
+  if (diff[0] > 0) return 'Major';
+  if (diff[1] > 0) return 'Minor';
+  if (diff[2] > 0) return 'Patch';
+
+  // No updates found
+  return false;
+}
+
+function extractVersion(version) {
+  const rawVersion = version.replaceAll(/[^0-9\.]/g, '');
+
+  if (!/^\d+\.\d+\.\d+$/.test(rawVersion)) {
+    console.log(`Invalid version format: ${rawVersion}`);
+    return '';
+  }
+
+  return rawVersion;
 }
